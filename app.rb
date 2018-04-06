1b10e0639ee7dc2c325b95d407def856f77c25fd
Alex McKean <amckean@me.com>
2018-04-06 14:17:23 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do 
+    "My Name is"
+  end 
+
+  get '/hometown' do 
+    "My Home town is"
+  end 
+
+  get '/favorite-song' do
+    "My favorite song is"
+  end 
 end
